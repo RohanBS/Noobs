f5f31beb348921f7f979b068ff51f0de02549751
Rohan_B_Sahu <rohanbs98@gmail.com>
2017-09-09 02:55:33 +0530

    Added record button-click to record for 1 second

--- MainActivity.java
+++ MainActivity.java
@@ -1,111 +1,78 @@
-package com.example.rohanbs.decibeltest;
+package com.example.rohanbs.soundtest;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.util.Log;
-import android.media.MediaRecorder;
-import java.io.File;
-import android.os.Environment;
 import android.os.Handler;
-import java.io.IOException;
-
+import android.util.Log;
+import android.widget.EditText;
+import android.media.MediaRecorder;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaRecorder recorder= new MediaRecorder();
+    MediaRecorder mRecorder;
+    public void startRecord(View view){
+        Log.i("Hello","sup");
+        if (mRecorder == null)
+        {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try
+            {
+                mRecorder.prepare();
+            }catch (java.io.IOException ioe) {
+                android.util.Log.e("[Monkey]", "IOException: " +
+                        android.util.Log.getStackTraceString(ioe));
 
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " +
+                        android.util.Log.getStackTraceString(e));
+            }
+            try
+            {
+                mRecorder.start();
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " +
+                        android.util.Log.getStackTraceString(e));
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (mRecorder != null) {
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+            }
+            Log.i("Hello","record complete");
+
+            //mEMA = 0.0;
+        }
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     // Example of a call to a native method
-
-
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    tv.setText(stringFromJNI());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText("Options menu created");
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            try{
-                startRecorder();
-            }catch(Exception e){
-                Log.e("Record Start","Failed");
-            }
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void startRecorder(){
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText("recording");
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile("/dev/null");
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            recorder.prepare();
-        } catch (IOException e) {
-            Log.e("Recording", "Record attempt failed");
-        }
-
-        recorder.start();
-        tv.setText("Actual start");
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Do something after 5s = 1000ms
-                stopRecorder();
-            }
-        }, 1000);
-
-
-    }
-    public void stopRecorder(){
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText("stopping");
-        recorder.stop();
-        recorder.reset();
-    }
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
